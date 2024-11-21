fcafacd3fa1044e7f2c7373bf882f101dd0c879a
Aditya-Karmalkar <126249135+Aditya-Karmalkar@users.noreply.github.com>
2024-11-21 17:22:12 +0000

    variable commit

---
+++ 01_variables.js
@@ -0,0 +1,19 @@
+accountId = 10;
+let accountEmail = '@google.com';
+var accountCity = 'Pune';
+
+accountId = 20;
+accountEmail = '1@google.com';
+accountCity = 'Pune';
+ 
+accountId = 20;
+accountEmail = '2@google.com';
+accountCity = 'Pune';
+ 
+accountId = 30;
+accountEmail = '3@google.com';
+accountCity = 'Pune';
+
+console.log(accountId + ' '+ accountEmail + ' ' + accountCity + ' ');
+
+console.table([accountId, accountEmail,accountCity]);
