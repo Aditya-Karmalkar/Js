427b61bc9cad308378c64a514079dceac347bb96
Aditya-Karmalkar <126249135+Aditya-Karmalkar@users.noreply.github.com>
2024-11-22 04:55:39 +0000

    Conversion of Datatypes

---
+++ 03_conversionOperations.js
@@ -0,0 +1,17 @@
+let score = "22AANANAN";
+
+console.log(typeof score);
+
+let valueInNumber = Number(score)
+console.log(typeof valueInNumber);
+console.log(valueInNumber);
+
+let isLoggedin = 0; //1 => True; 0 => False
+let booleanIsLoggedIn = Boolean(isLoggedin)
+console.log(typeof booleanIsLoggedIn);
+console.log(booleanIsLoggedIn);
+
+let someNumber = 4; 
+let stringSomeNumber = String(someNumber)
+console.log(typeof stringSomeNumber);
+console.log(stringSomeNumber);
