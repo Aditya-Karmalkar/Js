0cc28ad7bf5f1c582d240ea0a7519ccd065825f4
Aditya-Karmalkar <126249135+Aditya-Karmalkar@users.noreply.github.com>
2024-11-25 14:31:50 +0000

    commit

---
+++ 10_classes_and_oop/staticprop.js
@@ -0,0 +1,27 @@
+class User {
+    constructor(username){
+        this.username = username
+    }
+
+    logMe(){
+        console.log(`Username: ${this.username}`);
+    }
+
+    static createId(){
+        return `123`
+    }
+}
+
+const aditya = new User("aditya")
+// console.log(aditya.createId())
+
+class Teacher extends User {
+    constructor(username, email){
+        super(username)
+        this.email = email
+    }
+}
+
+const iphone = new Teacher("iphone", "i@phone.com")
+// console.log(iphone.username);
+iphone.logMe();
